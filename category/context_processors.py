c15d267b5a6f8b2c4944529b0a59aaa647042fcc
Ural_K <ural46@gmail.com>
2022-08-08 20:14:34 -0700

    fix

--- category/context_processors.py
+++ category/context_processors.py
@@ -3,6 +3,6 @@ from django.db.models import Avg, Count, Sum
 
 
 def active_categories(request):
-    objs = Category.objects.annotate(categoryitems=Count('item'))
+    objs = Category.objects.all().annotate(categoryitems=Count('item'))
     categories = objs.order_by('-categoryitems').filter(categoryitems__gt=0)
     return dict(categories=categories)
