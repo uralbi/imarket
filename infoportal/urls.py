ef07a9cea5ea439d0da2a3553fda0ba53b35c22d
Ural_K <ural46@gmail.com>
2022-08-08 18:59:24 -0700

    bug fix

--- infoportal/urls.py
+++ infoportal/urls.py
@@ -1,10 +1,10 @@
 import os
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 from django.views.static import serve
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import re_path
 from .settings import BASE_DIR
 from .views import HomePage
 
@@ -14,7 +14,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('items/', include('market.urls')),
     path('msgs/', include('chat.urls')),
-    url(r'^icons/(?P<path>.*)$', serve,
+    re_path(r'^icons/(?P<path>.*)$', serve,
         {'document_root': os.path.join(BASE_DIR, 'media/icons'),
          'show_indexes': True}, name='icons')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
